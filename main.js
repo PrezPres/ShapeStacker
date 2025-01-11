f2642e251a49e21c3d706b1cb796e258b37eccfa
PrezPres <nhagerty@gmail.com>
2025-01-10 20:53:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -185,7 +185,7 @@ function startExtraTime() {
   // Create a text message at the top center for the extra time countdown
   const extraTimeText = this.add.text(
     config.width / 2, // Center horizontally
-    40,               // Small margin from the top
+    -40,               // Small margin from the top
     `Extra Time: ${extraTime}`,
     {
       font: "24px Arial",
